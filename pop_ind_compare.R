52abf758440fe120594dd8b6ebd2e95cac0d1011
parksw3 <parksw3@mcmaster.ca>
2020-07-29 02:34:28 -0400

    some progress

--- pop_ind_compare.R
+++ pop_ind_compare.R
@@ -108,53 +108,83 @@ k_ind3 <- k_intrinsic3 %>%
 
 gbase <- ggplot(k_intrinsic1) +
   geom_polygon(aes(tau, den), fill="gray70") +
-  scale_x_continuous("Time since infection (days), $\\tau$", expand=c(0, 0), limits=c(0, 15)) +
-  scale_y_continuous("Effective kernel, $K_{\\textrm{\\tiny{eff}}}(t_{\\textrm{\\tiny{infect}}}, \\tau)$", expand=c(0, 0), limits=c(0, 0.499)) +
+  scale_x_continuous("Time since infection, $\\tau$ (days)", expand=c(0, 0), limits=c(0, 15)) +
+  scale_y_continuous("Forward kernel, $K_{t}(\\tau)$",
+                     breaks=c(0, 0.2, 0.4, 0.6, 0.8, 1),
+                     expand=c(0, 0), limits=c(0, 1.1),
+                     sec.axis = sec_axis(~., name="Relative changes in infection kernel", breaks=c(0, 0.5, 1))) +
   theme(
     panel.grid = element_blank()
   )
 
 g1 <- gbase +
   geom_polygon(data=k_pop1, aes(tau, keff), fill="darkred") +
+  geom_line(data=k_pop1, aes(tau, keff/den), lwd=1.5) +
   geom_vline(xintercept=sum(k_pop1$keff*k_pop1$tau)/sum(k_pop1$keff), col="black", lty=2) +
   geom_vline(xintercept=sum(k_intrinsic1$den*k_pop1$tau)/sum(k_intrinsic1$den), col="black", lty=1) +
-  annotate("text", x=Inf, y=Inf, label="$t_{\\textrm{\\tiny{infect}}}=t_{\\textrm{\\tiny{start}}}-5$",
-           hjust=1, vjust=1)
+  annotate("text", x=Inf, y=Inf, label="$t=t_{\\textrm{\\tiny{start}}}-5$",
+           hjust=1.1, vjust=1.1) +
+  theme(
+    axis.title.y.right = element_blank(),
+    axis.title.x = element_blank()
+  )
 
 g2 <- gbase +
   geom_polygon(data=k_pop2, aes(tau, keff), fill="darkred") +
+  geom_line(data=k_pop2, aes(tau, keff/den), lwd=1.5) +
   geom_vline(xintercept=sum(k_pop2$keff*k_pop1$tau)/sum(k_pop2$keff), col="black", lty=2) +
   geom_vline(xintercept=sum(k_intrinsic1$den*k_pop1$tau)/sum(k_intrinsic1$den), col="black", lty=1) +
-  annotate("text", x=Inf, y=Inf, label="$t_{\\textrm{\\tiny{infect}}}=t_{\\textrm{\\tiny{start}}}$",
-           hjust=1, vjust=1)
+  annotate("text", x=Inf, y=Inf, label="$t=t_{\\textrm{\\tiny{start}}}$",
+           hjust=1.1, vjust=1.1)  +
+  theme(
+    axis.title.y = element_blank(),
+    axis.title.x = element_blank()
+  )
 
 g3 <- gbase +
   geom_polygon(data=k_pop3, aes(tau, keff), fill="darkred") +
+  geom_line(data=k_pop3, aes(tau, keff/den), lwd=1.5) +
   geom_vline(xintercept=sum(k_pop3$keff*k_pop1$tau)/sum(k_pop3$keff), col="black", lty=2) +
   geom_vline(xintercept=sum(k_intrinsic1$den*k_pop1$tau)/sum(k_intrinsic1$den), col="black", lty=1) +
-  annotate("text", x=Inf, y=Inf, label="$t_{\\textrm{\\tiny{infect}}}=t_{\\textrm{\\tiny{end}}}-5$",
-           hjust=1, vjust=1)
+  annotate("text", x=Inf, y=Inf, label="$t=t_{\\textrm{\\tiny{end}}}-5$",
+           hjust=1.1, vjust=1.1)  +
+  theme(
+    axis.title.y.left = element_blank(),
+    axis.title.x = element_blank()
+  )
 
 g4 <- gbase +
   geom_polygon(data=k_ind1, aes(tau, keff), fill="darkblue") +
+  geom_line(data=k_ind1, aes(tau, keff/den), lwd=1.5) +
   geom_vline(xintercept=sum(k_ind1$keff*k_pop1$tau)/sum(k_ind1$keff), col="black", lty=2) +
   geom_vline(xintercept=sum(k_intrinsic1$den*k_pop1$tau)/sum(k_intrinsic1$den), col="black", lty=1) +
-  annotate("text", x=Inf, y=Inf, label="$t_{\\textrm{\\tiny{infect}}}=t_{\\textrm{\\tiny{start}}}-5$",
-           hjust=1, vjust=1)
+  annotate("text", x=Inf, y=Inf, label="$t=t_{\\textrm{\\tiny{start}}}-5$",
+           hjust=1.1, vjust=1.1)+
+  theme(
+    axis.title.y.right = element_blank()
+  )
 
 g5 <- gbase +
   geom_polygon(data=k_ind2, aes(tau, keff), fill="darkblue") +
+  geom_line(data=k_ind2, aes(tau, keff/den), lwd=1.5) +
   geom_vline(xintercept=sum(k_ind2$keff*k_pop1$tau)/sum(k_ind2$keff), col="black", lty=2) +
   geom_vline(xintercept=sum(k_intrinsic1$den*k_pop1$tau)/sum(k_intrinsic1$den), col="black", lty=1) +
-  annotate("text", x=Inf, y=Inf, label="$t_{\\textrm{\\tiny{infect}}}=t_{\\textrm{\\tiny{start}}}$",
-           hjust=1, vjust=1)
+  annotate("text", x=Inf, y=Inf, label="$t=t_{\\textrm{\\tiny{start}}}$",
+           hjust=1.1, vjust=1.1)  +
+  theme(
+    axis.title.y = element_blank()
+  )
 
 g6 <- gbase +
   geom_polygon(data=k_ind3, aes(tau, keff), fill="darkblue") +
+  geom_line(data=k_ind3, aes(tau, keff/den), lwd=1.5) +
   geom_vline(xintercept=sum(k_ind3$keff*k_pop1$tau)/sum(k_ind3$keff), col="black", lty=2) +
   geom_vline(xintercept=sum(k_intrinsic1$den*k_pop1$tau)/sum(k_intrinsic1$den), col="black", lty=1) +
-  annotate("text", x=Inf, y=Inf, label="$t_{\\textrm{\\tiny{infect}}}=t_{\\textrm{\\tiny{end}}}-5$",
-           hjust=1, vjust=1)
+  annotate("text", x=Inf, y=Inf, label="$t=t_{\\textrm{\\tiny{end}}}-5$",
+           hjust=1.1, vjust=1.1)  +
+  theme(
+    axis.title.y.left = element_blank()
+  )
 
 gtot <- ggarrange(g1, g2, g3, g4, g5, g6, nrow=2,
           labels=c("A", "B", "C",
